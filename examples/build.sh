97fd17788170d8ce98f82339374f8d79b5015e4d
Jon <john_akke@hotmail.com>
2020-02-17 20:18:32 +0100

    Improved build files for cpp files in example directory.

--- examples/build.sh
+++ examples/build.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
-mkdir build &&
-cd build &&
-cmake -Dhelib_DIR="${PWD}/../../lib/helib_pack/share/cmake/helib" .. &&
-make &&
-mv bgv_matrix_op .. &&
-cd .. && rm -rf build/
+if [ -e ${PWD}/bin ];
+then
+    echo "bin already exists."
+else
+    mkdir -v bin;
+fi
+
+if [ -e ${PWD}/build ];
+then
+    cd build
+else
+    mkdir -v build && cd build;
+fi
+
+if [ -e ${helib_DIR} ];
+then
+    cmake ..
+else
+    echo "set helib_DIR: export helib_DIR=/path/to/helib_pack/share/cmake/helib";
+fi
+
+if [ -e ${PWD}/Makefile ];
+then
+    make all && cd ..
+else
+    echo "No makefile.";
+fi
+
+if [ -e ${PWD}/build ];
+then
+    echo "Removing build files from "$(realpath build/);
+    rm -rI build/
+else
+    echo "Build does not exist.";
+fi
+
+
