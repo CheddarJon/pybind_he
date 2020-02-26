a3e6c567c8c4a65c4fd6d3c90e1c4ff73faaf212
CheddarJon <john_akke@hotmail.com>
2020-02-26 15:03:00 +0100

    Added testflag to helib

--- script/install_HElib.sh
+++ script/install_HElib.sh
@@ -6,6 +6,6 @@ mkdir ../submodules/HElib/build
 cd ../submodules/HElib/
 pwd
 #export CMAKE_INSTALL_PREFIX=/home/shit
-cmake -DPACKAGE_BUILD=ON -DCMAKE_INSTALL_PREFIX=/build
+cmake -DPACKAGE_BUILD=ON -DENABLE_TEST=ON -DCMAKE_INSTALL_PREFIX=/build
 make -j16
 make test
