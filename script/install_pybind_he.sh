531e3bf8f50a305d030b1547b398af58fc8637d3
Karl-Johan von Hacht <kjvonhacht@gmail.com>
2020-03-01 11:16:52 +0100

    update, for now choose if local or site-package install of pybind

--- script/install_pybind_he.sh
+++ script/install_pybind_he.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-python3 ../setup.py install --home="../lib"
+
+if [ $1 ]
+then
+	python3 ../setup.py install --home="../lib"
+else
+	python3 ../setup.py install
+fi
+
+rm -r -f build
