977a5987048fe3c158a9417f9223debddf521685
CheddarJon <john_akke@hotmail.com>
2020-03-06 16:38:52 +0100

    Changed fft implementation.

--- examples/tfhe_build.sh
+++ examples/tfhe_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gcc tutorial_tfhe.c -o bin/tutorial_tfhe -ltfhe-spqlios-fma
-gcc client_tfhe.c -o bin/client_tfhe -ltfhe-spqlios-fma
-gcc server_tfhe.c -o bin/server_tfhe -ltfhe-spqlios-fma
+gcc tutorial_tfhe.c -o bin/tutorial_tfhe -ltfhe-fftw
+gcc client_tfhe.c -o bin/client_tfhe -ltfhe-fftw
+gcc server_tfhe.c -o bin/server_tfhe -ltfhe-fftw
