79f18809a0c97109446d1045a1df616c09627b3c
CheddarJon <john_akke@hotmail.com>
2020-03-06 16:37:32 +0100

    Fixed bug in equals func. Eval supports and assumes a ciphertext file with two 16bit numbers.

--- examples/server_tfhe.c
+++ examples/server_tfhe.c
@@ -15,6 +15,15 @@ void compare_bit(LweSample* result, const LweSample* a, const LweSample* b,
 	bootsMUX(result, tmp, lsb_carry, a, ck);
 }
 
+void
+equal_bits(LweSample* result, const LweSample* a, const LweSample* b,
+		LweSample* tmp,
+		const TFheGateBootstrappingCloudKeySet* ck)
+{
+	bootsXNOR(tmp, a, b, ck);
+	bootsAND(result, result, tmp, ck);
+}
+
 // this function compares two multibit words, and puts the max in result
 void minimum(LweSample* result, const LweSample* a, const LweSample* b, const int bits,
 		const TFheGateBootstrappingCloudKeySet* ck)
@@ -36,6 +45,23 @@ void minimum(LweSample* result, const LweSample* a, const LweSample* b, const in
 	delete_gate_bootstrapping_ciphertext_array(2, tmps);
 }
 
+void
+equal(LweSample* result, const LweSample* a, const LweSample* b, const int bits,
+		const TFheGateBootstrappingCloudKeySet* ck)
+{
+	/* Returns 1 if a == b
+	 * Returns 0 if a != b
+	 */
+	LweSample* tmp = new_gate_bootstrapping_ciphertext(ck->params);
+	bootsCONSTANT(&result[0], 1, ck);
+
+	// compare all bits of a and b
+	for (uint8_t i = 0; i < bits; i++)
+		equal_bits(&result[0], &a[i], &b[i], tmp, ck);
+
+	delete_gate_bootstrapping_ciphertext(tmp);
+}
+
 int
 eval(EVAL_FUNC_PTR func, const int bits)
 {
@@ -46,22 +72,26 @@ eval(EVAL_FUNC_PTR func, const int bits)
 
 	const TFheGateBootstrappingParameterSet* params = ck->params;
 
-	LweSample* ciphertext = new_gate_bootstrapping_ciphertext_array(bits, params);
+	LweSample* ciphertext1 = new_gate_bootstrapping_ciphertext_array(bits, params);
+	LweSample* ciphertext2 = new_gate_bootstrapping_ciphertext_array(bits, params);
 	LweSample* result = new_gate_bootstrapping_ciphertext_array(bits, params);
 
 	FILE* data = fopen(CIPHER_FILE, READMODE);
 	for (uint8_t i = 0; i < bits; i++)
-		import_gate_bootstrapping_ciphertext_fromFile(data, &ciphertext[i], params);
+		import_gate_bootstrapping_ciphertext_fromFile(data, &ciphertext1[i], params);
+	for (uint8_t i = 0; i < bits; i++)
+		import_gate_bootstrapping_ciphertext_fromFile(data, &ciphertext2[i], params);
 	fclose(data);
 
-	func(result, ciphertext, ciphertext, bits, ck);
+	func(result, ciphertext1, ciphertext2, bits, ck);
 
 	FILE* output = fopen(DATA_FILE, WRITEMODE);
 	for (uint8_t i = 0; i < bits; i++)
 		export_gate_bootstrapping_ciphertext_toFile(output, &result[i], params);
 	fclose(output);
 
-	delete_gate_bootstrapping_ciphertext_array(bits, ciphertext);
+	delete_gate_bootstrapping_ciphertext_array(bits, ciphertext1);
+	delete_gate_bootstrapping_ciphertext_array(bits, ciphertext2);
 	delete_gate_bootstrapping_ciphertext_array(bits, result);
 	delete_gate_bootstrapping_cloud_keyset(ck);
 	return 0;
@@ -71,6 +101,6 @@ int
 main()
 {
 	EVAL_FUNC_PTR f;
-	f = minimum;
+	f = equal;
 	eval(f, 16);
 }
