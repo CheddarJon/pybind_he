4db328bf62a33d041a7f0295c9df3df2ab7ecd5f
Jon <john_akke@hotmail.com>
2020-03-06 11:15:45 +0100

    Added client that can generate keys and parameters, encrypt and decrypt 16bit messages. Updated the build file to work with the new file.

--- examples/client_tfhe.c
+++ examples/client_tfhe.c
@@ -2,29 +2,12 @@
 #include <tfhe/tfhe_io.h>
 #include <stdio.h>
 
-TFheGateBootstrappingSecretKeySet*
-generateKeys(const int security_bits, int export_keys, TFheGateBootstrappingParameterSet* params)
-{
-	/* Sets parameters according to security specs, exports the keys if export_keys is set.
-	 * Returns pointer to the secret key set and sets params.
-	 */
-	params = new_default_gate_bootstrapping_parameters(security_bits);
-
-	uint32_t seed[] = {314, 1592, 657};
-	tfhe_random_generator_setSeed(seed, 3);
-	TFheGateBootstrappingSecretKeySet* key = new_random_gate_bootstrapping_secret_keyset(params);
-
-	if (export_keys) {
-		FILE* secret_key = fopen("secret.key", "wb");
-		export_tfheGateBootstrappingSecretKeySet_toFile(secret_key, key);
-		fclose(secret_key);
-
-		FILE* cloud_key = fopen("cloud.key", "wb");
-		export_tfheGateBootstrappingCloudKeySet_toFile(cloud_key, &key->cloud);
-		fclose(cloud_key);
-	}
-	return key;
-}
+#define PARAMS "params.data"
+#define SECRETKEY "secret.key"
+#define CLOUDKEY "cloud.key"
+#define WRITEMODE "wb"
+#define READMODE "rb"
+#define CIPHER_FILE "ciphertext.data"
 
 int die(const char* msg)
 {
@@ -32,17 +15,116 @@ int die(const char* msg)
 	return -1;
 }
 
+int
+generate(const int security_bits, uint8_t export)
+{
+	/* Sets parameters according to security specs, exports the keys if export_keys is set.
+	 * Returns pointer to the secret key set and sets params.
+	 */
+	TFheGateBootstrappingParameterSet* params =
+		new_default_gate_bootstrapping_parameters(security_bits);
+	if (params == NULL)
+		return -2;
+
+	uint32_t seed[] = {314, 1592, 657};
+	tfhe_random_generator_setSeed(seed, 3);
+
+	TFheGateBootstrappingSecretKeySet* key =
+		new_random_gate_bootstrapping_secret_keyset(params);
+	if (key == NULL)
+		return -1;
+
+	if (export) {
+		FILE* parameters = fopen(PARAMS, WRITEMODE);
+		export_tfheGateBootstrappingParameterSet_toFile(parameters, params);
+		fclose(parameters);
+
+		FILE* secret_key = fopen(SECRETKEY, WRITEMODE);
+		export_tfheGateBootstrappingSecretKeySet_toFile(secret_key, key);
+		fclose(secret_key);
+
+		FILE* cloud_key = fopen(CLOUDKEY, WRITEMODE);
+		export_tfheGateBootstrappingCloudKeySet_toFile(cloud_key, &key->cloud);
+		fclose(cloud_key);
+	}
+
+	delete_gate_bootstrapping_parameters(params);
+	delete_gate_bootstrapping_secret_keyset(key);
+	return 0;
+}
+
+int
+encrypt16(int16_t msg, uint8_t export)
+{
+	uint8_t bits = 16;
+
+	FILE* secret_key = fopen(SECRETKEY, READMODE);
+	TFheGateBootstrappingSecretKeySet* key =
+		new_tfheGateBootstrappingSecretKeySet_fromFile(secret_key);
+	fclose(secret_key);
+
+	const TFheGateBootstrappingParameterSet* params = key->params;
+
+	LweSample* ciphertext = new_gate_bootstrapping_ciphertext_array(16, params);
+
+	for (uint8_t i = 0; i < bits; i++)
+		bootsSymEncrypt(&ciphertext[i], (msg >> i)&1, key);
+
+	if (export) {
+		FILE* encdata = fopen(CIPHER_FILE, WRITEMODE);
+		for (uint8_t i = 0; i < bits; i++)
+			export_gate_bootstrapping_ciphertext_toFile(encdata, &ciphertext[i], params);
+		fclose(encdata);
+	}
+
+	delete_gate_bootstrapping_secret_keyset(key);
+	delete_gate_bootstrapping_ciphertext_array(bits, ciphertext);
+	return 0;
+}
+
+int
+decrypt16()
+{
+	uint8_t bits = 16;
+	int16_t ret = 0;
+
+	FILE* secret_key = fopen(SECRETKEY, READMODE);
+	TFheGateBootstrappingSecretKeySet* key =
+		new_tfheGateBootstrappingSecretKeySet_fromFile(secret_key);
+	fclose(secret_key);
+
+	const TFheGateBootstrappingParameterSet* params = key->params;
+
+	LweSample* msg = new_gate_bootstrapping_ciphertext_array(bits, params);
+
+	FILE* data = fopen(CIPHER_FILE, READMODE);
+	for (uint8_t i = 0; i < bits; i++)
+		import_gate_bootstrapping_ciphertext_fromFile(data, &msg[i], params);
+
+	for (uint8_t i = 0; i < bits; i++) {
+		int tmp = bootsSymDecrypt(&msg[i], key);
+		ret |= (tmp << i);
+	}
+
+	delete_gate_bootstrapping_secret_keyset(key);
+	delete_gate_bootstrapping_ciphertext_array(bits, msg);
+	return ret;
+}
 
 int main(int argc, char **argv)
 {
-	TFheGateBootstrappingSecretKeySet* key;
-	TFheGateBootstrappingParameterSet* params;
+	int ret;
+	int16_t data = 2017;
 
-	if ((key = generateKeys(110, 0, params)) == NULL)
+	if ((ret = generate(110, 1)) == -1)
 	       die("Could not generate secret key set...");
 
-	if (params == NULL)
+	if (ret == -2)
 	       die("Could not set parameters...");
+
+	encrypt16(data, 1);
+	ret = decrypt16();
+	printf("Result of decryption = %d\n", ret);
 
 	return 0;
 }
--- examples/tfhe_build.sh
+++ examples/tfhe_build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 gcc tutorial_tfhe.c -o bin/tutorial_tfhe -ltfhe-spqlios-fma
+gcc client_tfhe.c -o bin/client_tfhe -ltfhe-spqlios-fma
