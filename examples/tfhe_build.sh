d1deb6a67c712dd6eb68306e8ebb1c2914c5fe80
Jon <john_akke@hotmail.com>
2020-03-10 13:09:57 +0100

    All tfhe related files from examples has been moved.

--- examples/tfhe_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-gcc -g -pg tutorial_tfhe.c -o bin/tutorial_tfhe -ltfhe-fftw
-gcc -g -pg client_tfhe.c -o bin/client_tfhe -ltfhe-fftw
-gcc -g -pg server_tfhe.c -o bin/server_tfhe -ltfhe-fftw
