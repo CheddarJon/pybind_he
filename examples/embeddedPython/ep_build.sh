0d7d89117afa78c67c44b9dce5dc42081ab0caa3
Jon <john_akke@hotmail.com>
2020-03-22 13:20:24 +0100

    Updated build script

--- examples/embeddedPython/ep_build.sh
+++ examples/embeddedPython/ep_build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cflags=$(python-config --cflags)
 ldflags=$(python-config --ldflags)
-gcc ${cflags} -o vhle_test ${ldflags} "-lpython3.8" vhle_test.c
-gcc ${cflags} -o pe_test ${ldflags} "-lpython3.8" pe_test.c
+gcc ${cflags} -o bin/vhle_test ${ldflags} "-lpython3.8" vhle_test.c
+gcc ${cflags} -o bin/pe_test ${ldflags} "-lpython3.8" pe_test.c
+gcc ${cflags} -o bin/eep ${ldflags} "-lpython3.8" eep.c
